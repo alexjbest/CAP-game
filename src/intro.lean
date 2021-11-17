7849f515fe4096dc36920a2e69329d48d0b77a93
Alex J Best <alex.j.best@gmail.com>
2021-11-17 16:43:30 +0100

    Initial commit

---
+++ src/intro.lean
@@ -0,0 +1,22 @@
+/-
+# The Topology Game, version 1.0.0.
+
+## By the Barcelona Lean Seminar
+
+Learn topology with Lean!
+
+# What is this game?
+
+In this game, you will learn topology and Lean at the same time. Travel through different worlds solving challenges. On the left side 
+of the screen you will find tactics and theorems you can use.
+
+Have fun!
+
+# Credits
+
+The Topology Game is brought to you by the Barcelona Lean Seminar, a project based at Universitat Autònoma de Barcelona.
+
+Lean is a computer theorem prover being developed at Microsoft Research.
+
+Prove a theorem. Write a function. <a href="https://twitter.com/XenaProject" target="blank">@XenaProject</a>.
+-/
