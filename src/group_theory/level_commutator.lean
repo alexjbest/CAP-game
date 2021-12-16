ca56ac0bfa4cfe0a1d7bfb92257a6831d966f4de
Alex J. Best <alex.j.best@gmail.com>
2021-12-16 10:35:48 +0100

    univ

--- src/group_theory/level_commutator.lean
+++ src/group_theory/level_commutator.lean
@@ -117,26 +117,6 @@ begin
 end
 
 
--- open tactic
--- meta def tactic.ac_refl' : tactic unit :=
--- do
---  (lhs, rhs) ← target >>= match_eq,
---    trace lhs,
---    s ← return $ cc_state.mk,
---    trace lhs,
---    s ← s.internalize lhs,
---    s ← s.internalize rhs,
---    trace lhs,
---    trace rhs,
---    b ← s.is_eqv lhs rhs,
---    trace $ s.pp_core ff,
---    trace b,
---    if b then do {
---      s.eqv_proof lhs rhs >>= exact
---    } else do {
---      fail "ac_refl failed"
---    }
-
 -- ex2
 /- Lemma
 -/
@@ -148,7 +128,6 @@ begin
   assoc_rw [mul_inv_self],
   rw mul_one,
   simp [mul_assoc],
-  -- ac_refl, -- fails
 end
 
 -- ex3
---
+++ src/group_theory/level_commutator_2.lean
@@ -0,0 +1,14 @@
+import .level_commutator
+
+-- ex2
+/- Lemma
+-/
+lemma commutator_mul {G : Type*} [group G] {x y z : G} : [x, z * y] = [x, y] * [x, z]^y :=
+begin
+  rw [commutator_def, commutator_def, commutator_def, conjugate_def, mul_inv_rev],
+  assoc_rw [mul_inv_self],
+  rw mul_one,
+  assoc_rw [mul_inv_self],
+  rw mul_one,
+  simp [mul_assoc],
+end
