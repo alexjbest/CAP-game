ad95c8019d82a786b959f88b9da3f3bad84bf998
Alex J. Best <alex.j.best@gmail.com>
2022-01-05 23:33:16 +0100

    elab

--- src/intro.lean
+++ src/intro.lean
@@ -5,11 +5,17 @@ Learn to use computers to formalize proofs!
 
 # What is this game?
 
-In this game, you will learn the basics of how a computer proof assistant works and formalize some
+In this game, you will learn the basics of how a program called a *proof assistant* works and use it to formalize some
 mathematical results.
-On the of the screen you will find tactics and theorems you can use.
-This is computer assisted proof in a very literal sense, using a computer to step through and
+On the left of the screen you will find *theorems* and *tactics* you can use to prove results.
+Using this very precise language we can work on proofs like normal, using definitions and already established facts to show new results.
+The proof assistant will keep track of what we know precisely, and show us what else needs to be shown to finish the current proof.
+
+This is a very literal sort of computer assisted proof, using a computer to step through and
 understand individual steps in a proof.
+Later we will see how the assistant can do more than just follow our instructions, it can find
+useful facts we might want to use for us, and even complete some moderately difficult proofs
+on its own, based on pre-defined rules.
 
 Have fun!
 
@@ -19,5 +25,5 @@ The CAP Game, was created as part of the course computer assisted proofs at the 
 
 Based on templates from Imperial College London and Universitat Autònoma de Barcelona.
 
-Lean is a computer theorem prover being developed at Microsoft Research.
+Lean is a proof assistant being developed at Microsoft Research.
 -/
